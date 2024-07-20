b9aa8b6a66b37e45fa9015c300a16712c792dbfa
Yael Chavez <you400@gmail.com>
2024-07-20 13:24:24 -0600

    ordering 2

---
+++ Ordering.Application/Extensions/ServiceRegistrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ordering.Application.Extensions
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static IServiceCollection RegisterApplication(this IServiceCollection services) 
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(x => x
+            .RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
---
+++ Ordering.Application/Features/Commands/Order/CreateOrder/CreateorderCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts;
+
+
+namespace Ordering.Application.Features.Commands.Order.CreateOrder
+{
+    public class CreateorderCommandHandler
+        (IGenericRepository<Domain.Entities.Order> repository, IMapper mapper)
+        : IRequestHandler<CreateOrderCommand, int>
+    {
+        public async Task<int> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = mapper.Map<Domain.Entities.Order>(request);
+            var newOrder = await repository.AddAsync(order);
+            return newOrder.Id;
+        }
+    }
+}
